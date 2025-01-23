cb7a08c300a8a9ff8ed1cf83c7924aa582f16029
Sulaksha M <>
2025-01-23 22:26:36 +0800

    echo

--- src/main/java/Sebastian.java
+++ src/main/java/Sebastian.java
@@ -1,20 +1,24 @@
+import java.util.Scanner;
 public class Sebastian {
-    /*
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }*/
-    public static void main(String[] args) {
-        System.out.println("--------------------------\n"
-        + "Hello! I'm Sebastian~ \n"
-        + "What can I do for you?\n"
-        + "--------------------------\n"
-        + "Bye! See you again :D\n"
-        + "--------------------------\n");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("     --------------------------\n"
+        + "     Hello! I'm Sebastian~ \n"
+        + "     What can I do for you?\n"
+        + "     --------------------------");
+        String input = s.nextLine();
+        while (!input.toUpperCase().equals("BYE")) {
+            System.out.println("     --------------------------\n"
+                    + "     "
+                    + input + "\n     --------------------------");
+            input = s.nextLine();
+        }
+        if (input.toUpperCase().equals("BYE")) {
+            System.out.println("     --------------------------\n"
+                    + "     Bye! See you again :D\n"
+                    + "     --------------------------\n");
+        }
 
     }
 }
